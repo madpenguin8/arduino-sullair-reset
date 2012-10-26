dbe27e80512b4b002f20c46a5602dfeaef49e89b
Mike Diehl <madpenguin8@yahoo.com>
2012-10-25 22:34:26 -0400

    Make more compact.
    Eliminate redundant code.

--- ard_sullair_controllers.cpp
+++ ard_sullair_controllers.cpp
@@ -7,12 +7,25 @@
 ///////////////////////////////////////////////////////
 
 // Serial messages excluding trailing checksum, they are generated later.
-char maintWarnMsg[] = {"01P54,2000,"};
-char oilFilterMsg[] = {"01P55,2000,"};
-char separatorMsg[] = {"01P56,8000,"};
-char airFilterMsg[] = {"01P57,2000,"};
-char oilMsg[] = {"01P58,8000,"};
-char oilAnalysisMsg[] = {"01P59,2000,"};
+//char maintWarnMsg[] = {"01P54,2000,"};
+//char oilFilterMsg[] = {"01P55,2000,"};
+//char separatorMsg[] = {"01P56,8000,"};
+//char airFilterMsg[] = {"01P57,2000,"};
+//char oilMsg[] = {"01P58,8000,"};
+//char oilAnalysisMsg[] = {"01P59,2000,"};
+
+//// Message Order ////
+// Maintenance Warning - 0
+// Oil Filter - 1
+// Separator - 2
+// Air Filter -3
+// Oil - 4
+// Oil Analysis - 5
+
+char* messages[] = {"01P54,2000,",
+    "01P55,2000,", "01P56,8000,",
+    "01P57,2000,", "01P58,8000,",
+    "01P59,2000,"};
 
 // Previous uptime value for timer.
 long previousMillis = 0;
@@ -26,11 +39,10 @@ void  setup()
     Serial.begin(9600);
 
     // Setup pin 2 as RTS out.
-    DDRD = DDRD | B00000100;
+    DDRD |= B00000100;
 
-    // Setup input pins 3-8
-    DDRD = DDRD & B00000111;
-    DDRB = DDRB & B11111110;
+    // Setup input pins 3-7
+    DDRD &= B00000111;
 
     // Set RTS LOW
     PORTD &= B11111011;
@@ -59,111 +71,43 @@ void readButtonInputs()
     // Return from the function on first high button
 
     if(PIND & B00001000){ // Pin 3
-        resetMaintenanceWarning();
+        sendResetMessage(1);
         return;
     }
 
     if(PIND & B00010000){ // Pin 4
-        resetOilFilter();
+        sendResetMessage(2);
         return;
     }
 
     if(PIND & B00100000){ // Pin 5
-        resetSeparator();
+        sendResetMessage(3);
         return;
     }
 
     if(PIND & B01000000){ // Pin 6
-        resetAirFilter();
+        sendResetMessage(4);
         return;
     }
 
     if(PIND & B10000000){ // Pin 7
-        resetOil();
-        return;
-    }
-
-    if(PINB & B00000001){ // Pin 8
-        resetOilAnalysis();
+        sendResetMessage(5);
         return;
     }
 }
 
-// Serial communications methods
-// Set RTS pin HIGH, transmit data then bring RTS back to LOW
-void resetMaintenanceWarning()
+void sendResetMessage(int selected)
 {
     // Set RS485 shifter to transmit
     PORTD |= B00000100;
     // Begin serial message
     Serial.write(2); // <STX> marks begining of message
-    Serial.print(maintWarnMsg);
-    Serial.print(checksum(maintWarnMsg), HEX);
-    Serial.println();
+    Serial.print(messages[selected]);
+    Serial.println(checksum(messages[selected]), HEX);
     Serial.flush();
     // Wait a millisecond
     delay(1);
     // Return RS485 shifter to receive
-    PORTD &= B11111011;
-}
-
-void resetOilFilter()
-{
-    PORTD |= B00000100;
-    Serial.write(2);
-    Serial.print(oilFilterMsg);
-    Serial.print(checksum(oilFilterMsg), HEX);
-    Serial.println();
-    Serial.flush();
-    delay(1);
-    PORTD &= B11111011;
-}
-
-void resetSeparator()
-{
-    PORTD |= B00000100;
-    Serial.write(2);
-    Serial.print(separatorMsg);
-    Serial.print(checksum(separatorMsg), HEX);
-    Serial.println();
-    Serial.flush();
-    delay(1);
-    PORTD &= B11111011;
-}
-
-void resetAirFilter()
-{
-    PORTD |= B00000100;
-    Serial.write(2);
-    Serial.print(airFilterMsg);
-    Serial.print(checksum(airFilterMsg), HEX);
-    Serial.println();
-    Serial.flush();
-    delay(1);
-    PORTD &= B11111011;
-}
-
-void resetOil()
-{
-    PORTD |= B00000100;
-    Serial.write(2);
-    Serial.print(oilMsg);
-    Serial.print(checksum(oilMsg), HEX);
-    Serial.println();
-    Serial.flush();
-    delay(1);
-    PORTD &= B11111011;
-}
-
-void resetOilAnalysis()
-{
-    PORTD |= B00000100;
-    Serial.write(2);
-    Serial.print(oilAnalysisMsg);
-    Serial.print(checksum(oilAnalysisMsg), HEX);
-    Serial.println();
-    Serial.flush();
-    delay(1);
     PORTD &= B11111011;
 }
 
